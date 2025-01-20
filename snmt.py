13564778f976ae6724599168e0180af0bc8401af
smirnovsrihards <134302554+smirnovsrihards@users.noreply.github.com>
2025-01-20 23:21:26 +0200

    Add files via upload

---
+++ snmt.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import socket
+import threading
+from scapy.all import Ether, ARP, TCP, UDP, ICMP, IP
+import argparse
+import textwrap
+import cmd
+
+class ARPScan:
+    pass
+
+class ICMPScan:
+    pass
+
+class TCPSniffer:
+    pass
+
+class UDPSniffer:
+    pass
+
+class Shell():
+    pass
+
+if __name__ == '__main__':
+    pass
