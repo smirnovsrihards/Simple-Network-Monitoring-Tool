3326905611a8f1efd1309a9761959b8cc4969f9e
smirnovsrihards <134302554+smirnovsrihards@users.noreply.github.com>
2025-04-22 14:12:30 +0300

    Update configer.py

--- network_monitor/core/configer.py
+++ network_monitor/core/configer.py
@@ -10,10 +10,20 @@ class ConfManager:
         subnet = config.get('Network', 'subnet')
         port_range = config.get('Network', 'port_range')
         db_name = config.get('Database', 'db_name')
-
+        port = config.get('SMTP', 'port')
+        smtp_server = config.get('SMTP', 'smtp_server')
+        sender_email = config.get('SMTP', 'sender_email')
+        receiver_email = config.get('SMTP', 'receiver_email')
+        app_password = config.get('SMTP', 'app_password')
+        
         config_values = {
             'subnet': subnet,
             'port_range': port_range,
             'db_name': db_name
+            'port': port,
+            'smtp_server': smtp_server,
+            'sender_email': sender_email,
+            'receiver_email': receiver_email,
+            'app_password': app_password
         }
         return config_values
